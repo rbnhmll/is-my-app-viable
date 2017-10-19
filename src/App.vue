60c4d0b88eb5d38b829358f990162e735940cf36
Robin Hamill <hamillrobin@gmail.com>
2017-10-19 15:00:25 -0400

    Conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <div id="app">
     <h1>Is my App viable?</h1>
     <IdeaForm v-if="this.stage === 1" :advanceStage="advanceStage" />
-    <DataProcessing v-if="this.stage === 2" />
-    <Results v-if="this.stage === 3" />
+    <DataProcessing
+      v-if="this.stage === 2"
+      :advanceStage="advanceStage"
+    />
+    <Results
+      v-if="this.stage === 3"
+      :resetStage="resetStage"
+    />
   </div>
 </template>
 
@@ -29,6 +35,9 @@ export default {
   methods: {
     advanceStage() {
       this.stage = this.stage + 1;
+    },
+    resetStage() {
+      this.stage = 1;
     }
   }
 }
