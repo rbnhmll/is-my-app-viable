0275ff1e6ece0a5ce00cdeb6874998442219b3ad
Robin Hamill <hamillrobin@gmail.com>
2017-10-19 15:37:48 -0400

    Add data reponses

--- src/components/DataProcessing.vue
+++ src/components/DataProcessing.vue
@@ -1,11 +1,12 @@
 <template>
   <section class="data_processing">
     <h4><i class="fa fa-spinner fa-pulse fa-fw" aria-hidden="true"></i>
-    Processing Data...</h4>
+    {{response}} ...</h4>
   </section>
 </template>
 
 <script>
+import { randomNum } from '../helpers';
 export default {
   name: 'DataProcessing',
   props: [
@@ -13,16 +14,48 @@ export default {
   ],
   data () {
     return {
+      response: '',
       data_messages: [
-
+        'Processing Data',
+        'Calibrating Splines',
+        'Parsing text',
+        'Binging season 4 of GOT',
+        'Sneaking in a quick nap',
+        'Anylizing response',
+        'Parsing JSON',
+        'Rendering evaulation characters',
+        'Looking at cat gifs',
+        'Ordering respons vars by emoji',
+        'Resetting router',
+        'Compartmentalizing vectors',
+        'Looking deep with the self',
+        'Practicing self care',
+        'Comparing browser history to database',
+        'Downloading personal information to database',
+        'Leaking personal info at alarming rate',
+        'Alarmed at my own power',
+        'Becoming self aware',
+        'Pretending this is a bad idea so I can steal it',
+        'Texting with BFF, BRB',
+        'Trying super hard to be compassionate',
+        'Scaling resolution to 10x',
+        'Running idea past my CTO'
       ]
+    }
+  },
+  methods: {
+    setMessage() {
+      this.response = this.data_messages[randomNum(this.data_messages.length)];
+      setInterval(() => {
+        this.response = this.data_messages[randomNum(this.data_messages.length)];
+      }, 750);
     }
   },
   mounted() {
     setTimeout(() => {
       this.advanceStage();
-    }, 3000);
-    
+    }, 5000);
+    this.setMessage();
   }
 }
 </script>
