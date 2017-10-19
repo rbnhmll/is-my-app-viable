ce3cc37755f0bf1f61631a283df64f455b8a7a22
Robin Hamill <hamillrobin@gmail.com>
2017-10-19 13:37:08 -0400

    Move AdvancedOptions to new component

---
+++ src/components/AdvancedOptions.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="advanced_options">
+    <div class="input_group">
+      <label for="advanced_options">Advanced Options</label>
+      <input type="checkbox" name="advanced_options" id="advanced_options">
+    </div>
+
+    <div class="">
+      <h2>Check all that apply</h2>
+
+      <label for="">Totally new idea!</label>
+      <input type="checkbox" name="" id="">
+
+      <label for="">Like Uber but _____</label>
+      <input type="checkbox" name="" id="">
+
+      <label for="">Disruptive</label>
+      <input type="checkbox" name="" id="">
+
+      <label for="">Profits over people</label>
+      <input type="checkbox" name="" id="">
+
+      <label for="">VC Approved</label>
+      <input type="checkbox" name="" id="">
+
+      <label for="">Zero capital</label>
+      <input type="checkbox" name="" id="">
+
+      <label for="">Came to you in a dream</label>
+      <input type="checkbox" name="" id="">
+      
+      <label for="">Sound investment</label>
+      <input type="checkbox" name="" id="">
+
+      <label for="">Easy to develope</label>
+      <input type="checkbox" name="" id="">
+      <div class="input_group">
+        <input type="submit" value="Submit">
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import friendly from "friendly-url";
+
+export default {
+  name: 'AdvancedOptions',
+  data () {
+    return {
+      checkboxes: [
+        "VC approved"
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/IdeaForm.vue
+++ src/components/IdeaForm.vue
@@ -3,47 +3,19 @@
     <label for="app_idea">App idea</label>
     <input type="text" name="app_idea" id="app_idea">
     
-    <div class="advanced_options">
-      <h2>Check all that apply</h2>
-
-      <label for="">Totally new idea!</label>
-      <input type="checkbox" name="" id="">
-
-      <label for="">Like Uber but _____</label>
-      <input type="checkbox" name="" id="">
-
-      <label for="">Disruptive</label>
-      <input type="checkbox" name="" id="">
-
-      <label for="">Profits over people</label>
-      <input type="checkbox" name="" id="">
-
-      <label for="">VC Approved</label>
-      <input type="checkbox" name="" id="">
-
-      <label for="">Zero capital</label>
-      <input type="checkbox" name="" id="">
-
-      <label for="">Came to you in a dream</label>
-      <input type="checkbox" name="" id="">
-      
-      <label for="">Sound investment</label>
-      <input type="checkbox" name="" id="">
-
-      <label for="">Easy to develope</label>
-      <input type="checkbox" name="" id="">
-
-      <input type="submit" value="Submit">
-    </div>
+    <AdvancedOptions />
   </form>
 </template>
 
 <script>
+import AdvancedOptions from "./AdvancedOptions"
 import friendly from "friendly-url";
-console.log(friendly);
 
 export default {
   name: 'IdeaForm',
+  components: {
+    AdvancedOptions
+  },
   data () {
     return {
       checkboxes: [
@@ -54,7 +26,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
