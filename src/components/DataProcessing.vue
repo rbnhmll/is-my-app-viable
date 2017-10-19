023e1ae62fa2e09f31e45e8f04766fa52dc8fb47
Robin Hamill <hamillrobin@gmail.com>
2017-10-19 17:23:30 -0400

    Add messages

--- src/components/DataProcessing.vue
+++ src/components/DataProcessing.vue
@@ -1,12 +1,17 @@
 <template>
   <section class="data_processing">
-    <h4><i class="fa fa-spinner fa-pulse fa-fw" aria-hidden="true"></i>
-    {{response}} ...</h4>
+    <!-- <h4><i class="fa fa-spinner fa-pulse fa-fw" aria-hidden="true"></i>
+    {{response}} ...</h4> -->
+    <ul class="data_processing__window">
+      <li v-for="(r, i) in response" :key="i">{{ r }}</li>
+    </ul>
   </section>
 </template>
 
 <script>
 import { randomNum } from '../helpers';
+let interval;
+
 export default {
   name: 'DataProcessing',
   props: [
@@ -14,41 +19,60 @@ export default {
   ],
   data () {
     return {
-      response: '',
+      response: [],
       data_messages: [
-        'Processing Data',
-        'Calibrating Splines',
-        'Parsing text',
-        'Binging season 4 of GOT',
-        'Sneaking in a quick nap',
-        'Anylizing response',
-        'Parsing JSON',
-        'Rendering evaulation characters',
-        'Looking at cat gifs',
-        'Ordering respons vars by emoji',
-        'Resetting router',
-        'Compartmentalizing vectors',
-        'Looking deep with the self',
-        'Practicing self care',
-        'Comparing browser history to database',
-        'Downloading personal information to database',
-        'Leaking personal info at alarming rate',
         'Alarmed at my own power',
+        'Analyzing response',
         'Becoming self aware',
+        'Binging season 4 of GOT',
+        'Buying low, selling high',
+        'Calibrating Splines',
+        'Checking with Bezos',
+        'Comparing browser history to database',
+        'Compartmentalizing vectors',
+        'Configuring webpack',
+        'Done. Nope, wait a sec',
+        'Downloading personal information to database',
+        'Just get through today, just get through today',
+        'Leaking personal info at alarming rate',
+        'Looking at cat gifs',
+        'Looking back on my life',
+        'Looking deep with the self',
+        'Ordering response vars by emoji',
+        'Parsing JSON',
+        'Parsing text',
+        'Practicing self care',
         'Pretending this is a bad idea so I can steal it',
-        'Texting with BFF, BRB',
-        'Trying super hard to be compassionate',
+        'Processing Data',
+        'Recalculating recalculations',
+        'Refactoring spaghetti code',
+        'Rendering evaluation characters',
+        'Resetting router',
+        'Reviewing pythagorean theorem',
+        'Running idea past my CTO',
         'Scaling resolution to 10x',
-        'Running idea past my CTO'
+        'Searching for missing comma',
+        'Sneaking in a quick nap',
+        'Txting w my BFF, BRB',
+        'Trying to be more compassionate',
+        'Evaluating structural integrity',
       ]
     }
   },
   methods: {
     setMessage() {
-      this.response = this.data_messages[randomNum(this.data_messages.length)];
-      setInterval(() => {
-        this.response = this.data_messages[randomNum(this.data_messages.length)];
-      }, 750);
+      this.response.push(this.data_messages[randomNum(this.data_messages.length)]);
+      interval = setInterval(() => {
+        this.response.push(this.data_messages[randomNum(this.data_messages.length)]);
+        this.updateScroll();
+      }, 250);
+    },
+    updateScroll() {    
+      var element = document.querySelector(".data_processing__window");
+      var isScrolledToBottom = element.scrollHeight - element.clientHeight <= element.scrollTop + 1;
+      if(isScrolledToBottom) {
+        element.scrollTop = element.scrollHeight - element.clientHeight;
+      }
     }
   },
   mounted() {
@@ -56,10 +80,22 @@ export default {
       this.advanceStage();
     }, 5000);
     this.setMessage();
+  },
+  beforeDestroy() {
+    clearInterval(interval);
   }
 }
 </script>
 
 <style scoped>
-
+.data_processing {
+  font-family: 'Inconsolata', monospace;
+  padding: 0;
+  margin: 0;
+  height: 100px;
+  overflow: scroll;
+}
+.data_processing li {
+  list-style: none;
+}
 </style>
