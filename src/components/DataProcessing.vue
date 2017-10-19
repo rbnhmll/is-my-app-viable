8a1e216656bccae57fe5666e26de2c4015929a43
Robin Hamill <hamillrobin@gmail.com>
2017-10-19 15:00:51 -0400

    Props, and mothods and setTimeout

--- src/components/DataProcessing.vue
+++ src/components/DataProcessing.vue
@@ -1,15 +1,28 @@
 <template>
   <section class="data_processing">
-    <h4>Data Processing</h4>
+    <h4><i class="fa fa-spinner fa-pulse fa-fw" aria-hidden="true"></i>
+    Processing Data...</h4>
   </section>
 </template>
 
 <script>
 export default {
   name: 'DataProcessing',
+  props: [
+    "advanceStage"
+  ],
   data () {
     return {
+      data_messages: [
+
+      ]
     }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.advanceStage();
+    }, 3000);
+    
   }
 }
 </script>
