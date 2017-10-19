563653619496bc68fe65b38ad784c493dc07cb96
Robin Hamill <hamillrobin@gmail.com>
2017-10-19 14:11:43 -0400

    Add Results comp

---
+++ src/components/Results.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="results">
+    <h4>Just stop</h4>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Results',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
