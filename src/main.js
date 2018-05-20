1f44f2ddfa0b202536d7106cb0f06c7927ff34c9
Robin Hamill <hamillrobin@gmail.com>
2018-05-20 11:52:16 -0400

    Reorder imports

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import friendly_url from 'friendly-url';
+
 import Vue from 'vue';
 import App from './App';
-
-import friendly_url from 'friendly-url';
 
 Vue.config.productionTip = false;
 
